afdeb66769008011eb82dfec8e90e4179f25d77c
Elshodev <elshodprofile@gmail.com>
2026-01-13 14:16:22 +0500

    change active

--- src/pages/Profile/components/DeviceInstructions.tsx
+++ src/pages/Profile/components/DeviceInstructions.tsx
@@ -50,30 +50,35 @@ const DeviceInstructions: React.FC<DeviceInstructionsProps> = ({
           alt=""
         />
         <div className="flex flex-col gap-[2px] font-medium w-full overflow-hidden">
-          {instructions.map((instruction, index) => (
-            <h6
-              key={index}
-              className="flex md:text-[16px] text-[14px] items-center gap-[5px]"
-            >
-              <svg
-                width="9"
-                height="9"
-                viewBox="0 0 9 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+          {instructions.map((instruction, index) => {
+            if (!link && index === 2) {
+              return;
+            }
+            return (
+              <h6
+                key={index}
+                className="flex md:text-[16px] text-[14px] items-center gap-[5px]"
               >
-                <rect
-                  y="4.24264"
-                  width="6"
-                  height="6"
-                  rx="0.781443"
-                  transform="rotate(-45 0 4.24264)"
-                  fill="black"
-                />
-              </svg>
-              {instruction}
-            </h6>
-          ))}
+                <svg
+                  width="9"
+                  height="9"
+                  viewBox="0 0 9 9"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    y="4.24264"
+                    width="6"
+                    height="6"
+                    rx="0.781443"
+                    transform="rotate(-45 0 4.24264)"
+                    fill="black"
+                  />
+                </svg>
+                {instruction}
+              </h6>
+            );
+          })}
         </div>
         <button className="self-start text-[#323D4A]" onClick={handleOpenModal}>
           <Info className="w-5 h-5" />
